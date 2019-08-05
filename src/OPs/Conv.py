d1f5f5335c4833836265462a0e37d8fcfc8beb47
ZQPei <ZQPei@github.com>
2019-08-05 15:31:35 +0800

    0.1.1

--- src/OPs/Conv.py
+++ src/OPs/Conv.py
@@ -3,6 +3,11 @@ import src.c2oObject as Node
 ##---------------------------------------------------Conv层-------------------------------------------------------##
 #获取超参数
 def getConvAttri(layer):
+    ##膨胀系数dilations
+    dilations = [1, 1]
+    if layer.convolution_param.dilation != []:
+        dilation = layer.convolution_param.dilation[0]
+        dilations = [dilation, dilation]
     ##填充pads
     pads = [0, 0, 0, 0]  # 默认为0
     if layer.convolution_param.pad != []:  # 若存在pad,则根据pad赋值
@@ -24,7 +29,7 @@ def getConvAttri(layer):
 
     # 超参数字典
     dict = {  # "auto_pad":"NOTSET",
-        "dilations": [1, 1, 1],
+        "dilations": dilations,
         "group": group,
         "kernel_shape": kernel_shape,
         "pads": pads,
@@ -33,6 +38,7 @@ def getConvAttri(layer):
     return dict
 #计算输出维度
 def getConvOutShape(input_shape,layer,dict):
+    dilations = dict["dilations"]
     kernel_shape = dict["kernel_shape"]
     pads = dict["pads"]
     strides = dict["strides"]
@@ -40,14 +46,23 @@ def getConvOutShape(input_shape,layer,dict):
     kernel_num = layer.convolution_param.num_output
 
     #计算输入维度output_shape
-    h = (input_shape[0][2] - kernel_shape[0] + 2 * pads[0])/strides[0] + 1 # 输出维度N= ((输入维度I - 卷积核维度K + 2 * 填充P)/步长S) + 1
+    h = (input_shape[0][2] - kernel_shape[0] + 2 * pads[0] - (kernel_shape[0]-1)*(dilations[0]-1))/strides[0] + 1 # 输出维度N= ((输入维度I - 卷积核维度K + 2 * 填充P - (卷积核维度-1)*(膨胀系数-1))/步长S) + 1
     #当h非整数 ,且未设置pad ,在遇到输出为非整数情况 ,向上取整 ,即在右边和下边补1
     if h > int(h) and layer.convolution_param.pad == []:
         output_shape_h = int(h) + 1
         pads = [0,0,1,1]
     else:
         output_shape_h = int(h)
-    output_shape = [[input_shape[0][0],kernel_num,output_shape_h,output_shape_h]]
+    
+    w = (input_shape[0][3] - kernel_shape[1] + 2 * pads[1] - (kernel_shape[1]-1)*(dilations[1]-1))/strides[1] + 1 # 输出维度N= ((输入维度I - 卷积核维度K + 2 * 填充P - (卷积核维度-1)*(膨胀系数-1))/步长S) + 1
+    #当h非整数 ,且未设置pad ,在遇到输出为非整数情况 ,向上取整 ,即在右边和下边补1
+    if w > int(w) and layer.convolution_param.pad == []:
+        output_shape_w = int(w) + 1
+        pads = [0,0,1,1]
+    else:
+        output_shape_w = int(w)
+
+    output_shape = [[input_shape[0][0],kernel_num,output_shape_h,output_shape_w]]
 
     return output_shape
 #构建节点
